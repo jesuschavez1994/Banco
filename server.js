81fe2923072b31ca6cd0cb638501dc687bc837b2
jesus Chavez <rc620620@gmail.com>
2020-07-16 18:50:19 -0400

    intsalando express y corrigiendo el directorio de enrutamiento dentro de la carpeta dist

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 require('./src/config/config');
 
 
-app.use(express.static(__dirname + '/dist/foundussweb'));
+app.use(express.static(__dirname + '/dist/BancoDeProductos'));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/foundussweb/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/BancoDeProductos/index.html'));
 });
 
  
