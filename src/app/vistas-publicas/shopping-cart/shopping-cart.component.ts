0bbd181703aaa9cb204d357b15b78cfeeb4d914d
Christopher <christopherdallar1234@gmail.com>
2020-11-21 22:10:18 -0400

    WIP: Corrigiendo posibles desfiguraciones de la maqueta por exceso de texto y agregando responsive para movil.

--- src/app/vistas-publicas/shopping-cart/shopping-cart.component.ts
+++ src/app/vistas-publicas/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('menuContainerFixed') menuContainerFixed: ElementRef;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+
+    console.warn('<!-- Component Code --!>');
+
+    console.warn('<!-- End Component Code --!>');
+
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  public activateMenu( $event: Event){
+
+    const menuContainerFixed = this.menuContainerFixed.nativeElement;
+    const pxTopElement = menuContainerFixed.offsetTop;
+    const pxTopDocument = document.documentElement.scrollTop;
+
+    console.log(pxTopElement);
+
+    console.log('target');
+    console.log(pxTopDocument);
+
+    if ( pxTopDocument > pxTopElement ) {
+      menuContainerFixed.classList.add( 'responsive-menu-container--fixed' );
+    } else {
+      menuContainerFixed.classList.remove( 'responsive-menu-container--fixed' );
+    }
+
+
+    // if($event.srcElement.children[0].scrollTop){
+
+
+    // }
+
   }
 
 }
