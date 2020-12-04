0bc9219e6d42987e9ab3bf4a798f0d65004347f0
Christopher <christopherdallar1234@gmail.com>
2020-12-04 05:36:33 -0400

    WIP: searcha-bar component ahora recibe y emite el evento necesario para controlar el sidebar desde el button sidebar list.
    Además, ahora el componente tiene la opción de mostrar el botón o no

--- src/app/shared/search-bar/search-bar.component.ts
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Input() buttonSidebarList = false;
+  @Input() isExpanded = false;
+  @Output() sidebarExpand = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggleSidebarList(event){
+    this.isExpanded = event;
+    this.sidebarExpand.emit( this.isExpanded );
+
   }
 
 }
