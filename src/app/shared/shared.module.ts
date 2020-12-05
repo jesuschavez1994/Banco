492979076938b6d75b37d0b29cc714989fbe7af9
Christopher <christopherdallar1234@gmail.com>
2020-12-05 15:24:13 -0400

    WIP: Al componenete image-preview-gallery estoy intentando agregar el efecto de zoom al hacer hover con el cursor, con el paquete ngx-image-zoom

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { BannerComponent } from './banner/banner.component';
 import { ProductsCardsComponent } from './products-cards/products-cards.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ImagePreviewGalleryComponent } from './image-preview-gallery/image-preview-gallery.component';
+import { NgxImageZoomModule } from 'ngx-image-zoom';
 
 @NgModule({
     declarations: [
@@ -31,7 +32,10 @@ import { ImagePreviewGalleryComponent } from './image-preview-gallery/image-prev
         ImagePreviewGalleryComponent,
 
     ],
-    imports: [ APP_ROUTING, PipesModule, CommonModule],
+    imports: [
+      APP_ROUTING, PipesModule,
+      CommonModule, NgxImageZoomModule
+    ],
     exports: [
       NavbarComponent, NavbarToHomeComponent,
       CategoriasComponent, NavbarstoreComponent,
