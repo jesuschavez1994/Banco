94f90946803fd0954fc87f89165218269edeb9ae
Javieradel <ashair455@gmail.com>
2020-12-30 19:07:56 -0430

    \n Commit Inicial \n Maquetado Home

--- src/app/vistas-publicas/home/home.component.ts
+++ src/app/vistas-publicas/home/home.component.ts
@@ -2,7 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { StoreService } from '@services/store/store.service';
 import { DataProductDB, Image } from '@interfaces/InterfaceProducto';
 import { ProductosLoads } from '@interfaces/InterfaceProducto';
-
+/*
+  header{
+    brand
+    link categoria
+    search bar
+    btn cart
+    user settings
+    user register
+    user login
+    avatar
+  }
+  banner{
+    slider jpg´s
+  }
+  
+*/
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
