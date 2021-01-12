94b73ef2d9309c4c63b375281e5b61a350cdf623
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-12 00:02:20 -0400

    Agregada linea con la direccion del servidor de produccion.

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,6 @@
-
 // tslint:disable-next-line: eofline
-export const URL_SERVICIOS = 'http://192.168.0.130/founduss/public';
+// Desarrollo
+// export const URL_SERVICIOS = 'http://192.168.0.130/founduss/public';
+
+// Producción
+export const URL_SERVICIOS = 'http://31.220.108.209';
