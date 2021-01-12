6439687ca2cef0c829ab92fb0b0f8eec34fe3b10
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-12 00:06:01 -0400

    Componente que muestra el comprobante de pago. (Ajuste de estilos para evitar que los estilos globales afecten el componente)

---
+++ src/app/vistas-privadas/settings/components/payment-details/payment-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Plan } from '../../models/plan';
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styleUrls: ['./payment-details.component.css'],
+})
+export class PaymentDetailsComponent implements OnInit {
+  @Output() pageChange: EventEmitter<string>;
+
+  nextPage = 'plans';
+
+  backToPreviousPage(): void {
+    this.nextPage = 'plans';
+    this.pageChange.emit(this.nextPage);
+    window.scrollTo(0, 0);
+  }
+
+  constructor() {
+    this.pageChange = new EventEmitter<string>();
+  }
+
+  ngOnInit(): void {}
+}
