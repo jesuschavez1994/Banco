f03c3f46dd3abad420ca82e1e1303a74d739f0b5
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-12 00:05:15 -0400

    Ajuste de estilos CSS para evitar que los estilos globales interfieran con el componente.

--- src/app/vistas-privadas/settings/components/plan-details/plan-details.component.ts
+++ src/app/vistas-privadas/settings/components/plan-details/plan-details.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Plan } from '../../models/plan';
 
 @Component({
   selector: 'app-plan-details',
   templateUrl: './plan-details.component.html',
-  styleUrls: ['./plan-details.component.css']
+  styleUrls: ['./plan-details.component.css'],
 })
 export class PlanDetailsComponent implements OnInit {
+  @Input() selectedPlanDetails: Plan;
+  @Output() pageChange: EventEmitter<string>;
 
-  constructor() { }
+  nextPage = 'plans';
+  webpayDebitCard = false;
 
-  ngOnInit(): void {
+  backToPreviousPage(): void {
+    this.nextPage = 'plans';
+    this.pageChange.emit(this.nextPage);
+    window.scrollTo(0, 0);
   }
 
+  toNextPage(): void {
+    this.nextPage = 'payment-details';
+    this.pageChange.emit(this.nextPage);
+    window.scrollTo(0, 0);
+  }
+
+  constructor() {
+    this.pageChange = new EventEmitter<string>();
+  }
+
+  ngOnInit(): void {}
 }
