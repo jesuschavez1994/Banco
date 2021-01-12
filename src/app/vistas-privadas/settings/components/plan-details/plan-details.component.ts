76fe04c0d7e0e45c0707d57db9e6eea7186fc051
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-11 21:32:40 -0400

    Creacion de componente que servira como vista de revision de los datos para adquirir un plan.

---
+++ src/app/vistas-privadas/settings/components/plan-details/plan-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-details',
+  templateUrl: './plan-details.component.html',
+  styleUrls: ['./plan-details.component.css']
+})
+export class PlanDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
