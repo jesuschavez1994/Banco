83993570e34efa0e9d9b36fe06a35e1c031d9edf
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-15 13:07:49 -0400

    Traspaso de estilos a la vista de planes.

--- src/app/vistas-privadas/account-settings/settings.component.ts
+++ src/app/vistas-privadas/account-settings/settings.component.ts
@@ -1,104 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Plan } from './models/plan';
-import { SubscriptionService } from '@services/subscription/subscription.service';
-
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css'],
-  animations: [
-    trigger('inOutAnimation', [
-      transition(':enter', [
-        style({ opacity: 0, zIndex: 100, position: 'absolute', top: 0 }),
-        animate(
-          '1s ease-out',
-          style({ opacity: 1, zIndex: 100, position: 'absolute', top: 0 })
-        ),
-      ]),
-      transition(':leave', [
-        style({ opacity: 1, zIndex: 100, position: 'absolute', top: 0 }),
-        animate(
-          '0.5s ease-in',
-          style({ opacity: 0, zIndex: 100, position: 'absolute', top: 0 })
-        ),
-      ]),
-    ]),
-  ],
 })
 export class SettingsComponent implements OnInit {
-  currentPage = 'plans';
-  selectedPlan: Plan;
+  constructor() {}
 
-  plans: Array<Plan>;
-  route = 'Planes';
-
-  // Processing the events data from the <app-product-card></app-product-card> component.
-  onPlanSelected(plan: Plan): void {
-    this.selectedPlan = plan;
-  }
-
-  onPageChange(page: string): void {
-    this.currentPage = page;
-  }
-
-  constructor(private subscriptionDataService: SubscriptionService) {}
-
-  ngOnInit(): void {
-    this.plans = [
-      new Plan(
-        'basic',
-        true,
-        false,
-        false,
-        false,
-        14.99,
-        'Mensual',
-        { amount1: true, amount2: false, amount3: false },
-        49.99,
-        true
-      ),
-      new Plan(
-        'standard',
-        true,
-        true,
-        false,
-        false,
-        24.99,
-        'Mensual',
-        { amount1: true, amount2: true, amount3: false },
-        49.99
-      ),
-      new Plan(
-        'premium',
-        true,
-        true,
-        true,
-        true,
-        29.99,
-        'Mensual',
-        { amount1: true, amount2: true, amount3: true },
-        169.99
-      ),
-    ];
-
-    this.selectedPlan = new Plan(
-      'basic',
-      true,
-      false,
-      false,
-      false,
-      14.99,
-      'Mensual',
-      { amount1: true, amount2: false, amount3: false },
-      49.99
-    );
-  }
+  ngOnInit(): void {}
 }
