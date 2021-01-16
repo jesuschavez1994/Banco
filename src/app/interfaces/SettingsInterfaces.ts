3dfc2aff834b5477e7ae8bc9b3cfc4fc7d55c759
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-15 20:15:01 -0400

    Creadas interfaces para el servicio de suscripcion.

---
+++ src/app/interfaces/SettingsInterfaces.ts
@@ -0,0 +1,45 @@
+/* 
+	Interfaces usadas en la vista "Configuración".
+*/
+
+/* 
+	Interfaces usadas para crear un pago con el servicio Webpay Plus.
+*/
+export interface OrderNumberCreation {
+  plan_name: string;
+  type: string;
+  price: number;
+  strore_id: number;
+}
+
+export interface CreatedOrder {
+  user_id: number;
+  title: string;
+  total_price: number;
+  type: string;
+  updated_at: string;
+  created_at: string;
+  id: number;
+}
+
+export interface Payment {
+  order_id: number;
+  type: string;
+  amount: number;
+  updated_at: string;
+  created_at: string;
+  // ID de la orden realizada
+  id: number; //Payment ID
+}
+
+export interface WebpayPayment {
+  payment_id: number;
+  user_id: number;
+}
+
+export interface PaymentCredentials {
+  token: string;
+  url: string;
+}
+
+export interface PaymentInfo {}
