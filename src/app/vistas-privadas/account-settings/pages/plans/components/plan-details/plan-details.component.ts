ab699b5edc72895cc49bed51a662b0f4ac56bf67
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-16 02:35:49 -0400

    Implementación de interfaces.

--- src/app/vistas-privadas/account-settings/pages/plans/components/plan-details/plan-details.component.ts
+++ src/app/vistas-privadas/account-settings/pages/plans/components/plan-details/plan-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Plan } from '../../models/plan';
+import { SubscriptionService } from '@services/subscription/subscription.service';
+import { CreatedOrder, Payment } from '@interfaces/SettingsInterfaces';
 
 @Component({
   selector: 'app-plan-details',
@@ -8,6 +10,7 @@ import { Plan } from '../../models/plan';
 })
 export class PlanDetailsComponent implements OnInit {
   @Input() selectedPlanDetails: Plan;
+  @Input() orderDetails: CreatedOrder;
   @Output() pageChange: EventEmitter<string>;
 
   nextPage = 'plans';
@@ -25,7 +28,7 @@ export class PlanDetailsComponent implements OnInit {
     window.scrollTo(0, 0);
   }
 
-  constructor() {
+  constructor(private subscriptionDataService: SubscriptionService) {
     this.pageChange = new EventEmitter<string>();
   }
 
