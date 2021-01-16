38c1288fa3a2830c7ad032d942d7a4b9cf3bfb5a
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-16 12:14:06 -0400

    Implementacion de peticiones usando Observables.

--- src/app/services/subscription/subscription.service.ts
+++ src/app/services/subscription/subscription.service.ts
@@ -2,12 +2,20 @@
 Servicio donde se implementará la lógica referente a la suscripción de los planes y sus respectivas pasarelas de pago. 
 */
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { URL_SERVICIOS } from 'src/app/config/config';
 import { BROWSER_STORAGE } from '../../browserStorage';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
 import {
   OrderNumberCreation,
   CreatedOrder,
+  Payment,
 } from '@interfaces/SettingsInterfaces';
 
 @Injectable({
@@ -23,7 +31,7 @@ export class SubscriptionService {
   private userId = this.localStorage.getItem('id');
   private token = this.localStorage.getItem('token');
 
-  public createOrderNumber(
+  /*   public createOrderNumber(
     planDetails: OrderNumberCreation
   ): Promise<CreatedOrder> {
     const url = `${this.apiBaseURL}/api/users/${this.userId}/orders`;
@@ -39,10 +47,51 @@ export class SubscriptionService {
       .toPromise()
       .then((response) => response as CreatedOrder)
       .catch(this.handleError);
+  } */
+
+  createOrderNumber(planDetails: OrderNumberCreation) {
+    const url = `${this.apiBaseURL}/api/users/${this.userId}/orders`;
+    // We add the corresponding headers to the request
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.token}`,
+      }),
+    };
+
+    return this.httpService
+      .post<CreatedOrder>(url, planDetails, httpOptions)
+      .pipe(catchError(this.handleError));
   }
 
-  private handleError(error: any) {
-    console.error('Something has gone wrong: ', error);
-    return Promise.reject(error.message || error);
+  /*   paymentCreation(userId: number, orderId: number): Promise<Payment> {
+    const url = `${this.apiBaseURL}/api/users/${userId}/orders/${orderId}/payments`;
+    // We add the corresponding headers to the request
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.token}`,
+      }),
+    };
+
+    return this.httpService
+      .post(url, httpOptions)
+      .toPromise()
+      .then((response) => response as Payment)
+      .catch(this.handleError);
+  } */
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Manejamos los errores que ocurran del lado del cliente.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // El servidor respondió con un codigo no satisfactorio.
+      // EL cuerpo de la respuesta contiene la información de lo que salió mal.
+      console.error(
+        `El servidor regresó el codigo: ${error.status}, ` +
+          `el cuerpo de la respuesta fue:: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Algo malo ha sucedido, por intente de nuevo mas tarde.');
   }
 }
