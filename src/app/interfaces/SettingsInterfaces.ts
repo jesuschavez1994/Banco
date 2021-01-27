8d1321675d5eabdecca0b1f5bf5cb753647867dd
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-01-27 00:30:14 -0400

    Agregado método para obtener el voucher de una transacción.

--- src/app/interfaces/SettingsInterfaces.ts
+++ src/app/interfaces/SettingsInterfaces.ts
@@ -51,4 +51,84 @@ export interface PaymentCredentials {
   url: string;
 }
 
-export interface PaymentInfo {}
+export interface VoucherDetails {
+  id: number;
+  order_id: number;
+  amount: number;
+  status: string;
+  type: string;
+  created_at: string;
+  updated_at: string;
+  webpayplusnormaltransaction: {
+    id: number;
+    payment_id: number;
+    token: string;
+    url: string;
+    error: number;
+    created_at: string;
+    updated_at: string;
+    webpayplusnormalresponse: {
+      id: number;
+      webpayplus_n_transaction_id: number;
+      vci: string;
+      amount: number;
+      status: string;
+      buy_order: string;
+      session_id: string;
+      card_detail: string;
+      accounting_date: string;
+      transaction_date: string;
+      authorization_code: string;
+      payment_type_code: string;
+      response_code: string;
+      installments_amount: number;
+      installments_number: number;
+      balance: number;
+      created_at: string;
+      updated_at: string;
+    };
+  };
+  webpayplusmalltransaction: number;
+}
+
+[
+  {
+    id: 31,
+    order_id: 92,
+    amount: 300,
+    status: 'FINISH_SUCCESS',
+    type: 'webpayplus',
+    created_at: '2021-01-27T03:46:08.000000Z',
+    updated_at: '2021-01-27T03:48:10.000000Z',
+    webpayplusnormaltransaction: {
+      id: 23,
+      payment_id: 31,
+      token: '01abd6d68f5b8fd2711e793e98a93576ff41984d261625f0782d70bf0dd9e10c',
+      url: 'https://webpay3gint.transbank.cl/webpayserver/initTransaction',
+      error: null,
+      created_at: '2021-01-27T03:46:10.000000Z',
+      updated_at: '2021-01-27T03:46:10.000000Z',
+      webpayplusnormalresponse: {
+        id: 11,
+        webpayplus_n_transaction_id: 23,
+        vci: 'TSY',
+        amount: 300,
+        status: 'AUTHORIZED',
+        buy_order: '92',
+        session_id: '1',
+        card_detail: '{"card_number":"6623"}',
+        accounting_date: '0127',
+        transaction_date: '2021-01-27T03:46:09.881Z',
+        authorization_code: '1213',
+        payment_type_code: 'VN',
+        response_code: '0',
+        installments_amount: null,
+        installments_number: 0,
+        balance: null,
+        created_at: '2021-01-27T03:48:10.000000Z',
+        updated_at: '2021-01-27T03:48:10.000000Z',
+      },
+    },
+    webpayplusmalltransaction: null,
+  },
+];
