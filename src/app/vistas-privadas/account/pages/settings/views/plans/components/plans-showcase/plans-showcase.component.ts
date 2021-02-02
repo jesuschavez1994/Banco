906761141fc67800910c6b506f6236aa63a5b6f9
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-02 01:36:31 -0400

    Creadas instancias actualizadas de la clase relacionada con los planes.

--- src/app/vistas-privadas/account/pages/settings/views/plans/components/plans-showcase/plans-showcase.component.ts
+++ src/app/vistas-privadas/account/pages/settings/views/plans/components/plans-showcase/plans-showcase.component.ts
@@ -15,51 +15,61 @@ export class PlansShowcaseComponent implements OnInit {
   ngOnInit(): void {
     this.plans = [
       new Plan(
+        0,
         'basic',
-        true,
-        false,
-        false,
-        false,
-        14,
-        'Mensual',
-        { amount1: true, amount2: false, amount3: false },
-        49,
-        true
+        6610,
+        [
+          { description: 'Carga de 100 productos.', available: true },
+          { description: 'Horario de negocio.', available: true },
+          { description: 'Radio de despacho.', available: true },
+          { description: 'Pasarela de pago.', available: true },
+        ],
+        [{ index: 0, amountOfPictures: 100, price: 58712 }]
       ),
       new Plan(
+        1,
         'standard',
-        true,
-        true,
-        false,
-        false,
-        24,
-        'Mensual',
-        { amount1: true, amount2: true, amount3: false },
-        49
+        11999,
+        [
+          { description: 'Carga de 100 productos.', available: true },
+          { description: 'Horario de negocio.', available: true },
+          { description: 'Radio de despacho.', available: true },
+          { description: 'Pasarela de pago.', available: true },
+        ],
+        [
+          { index: 0, amountOfPictures: 100, price: 58712 },
+          { index: 1, amountOfPictures: 300, price: 110999 },
+        ]
       ),
       new Plan(
+        2,
         'premium',
-        true,
-        true,
-        true,
-        true,
-        29,
-        'Mensual',
-        { amount1: true, amount2: true, amount3: true },
-        169
+        14678,
+        [
+          { description: 'Carga de 100 productos.', available: true },
+          { description: 'Horario de negocio.', available: true },
+          { description: 'Radio de despacho.', available: true },
+          { description: 'Pasarela de pago.', available: true },
+        ],
+        [
+          { index: 0, amountOfPictures: 100, price: 58712 },
+          { index: 1, amountOfPictures: 300, price: 110999 },
+          { index: 2, amountOfPictures: '300+', price: 146780 },
+        ]
       ),
     ];
 
     this.selectedPlan = new Plan(
+      0,
       'basic',
-      true,
-      false,
-      false,
-      false,
-      14.99,
-      'Mensual',
-      { amount1: true, amount2: false, amount3: false },
-      49.99
+      6610,
+      [
+        { description: 'Carga de 100 productos.', available: true },
+        { description: 'Horario de negocio.', available: true },
+        { description: 'Radio de despacho.', available: true },
+        { description: 'Pasarela de pago.', available: true },
+      ],
+      [{ index: 1, amountOfPictures: 100, price: 58712 }]
     );
   }
 }
