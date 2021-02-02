1044352e3496a22e089777a6c09554d84315ce69
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-02 01:32:36 -0400

    Mejorada la definicion de los atributos de la clase Plan.

--- src/app/vistas-privadas/account/pages/settings/views/plans/models/plan.ts
+++ src/app/vistas-privadas/account/pages/settings/views/plans/models/plan.ts
@@ -1,18 +1,12 @@
+import { BonusDetails, ProductBankDetails } from '../interfaces/Plans';
+
 export class Plan {
   constructor(
+    public id: number,
     public name: string,
-    public productLoad: boolean,
-    public contactInfo: boolean,
-    public bussinessHours: boolean,
-    public geoLocation: boolean,
-    public price: number,
-    public payType: string,
-    public productsPics: {
-      amount1?: boolean;
-      amount2?: boolean;
-      amount3?: boolean;
-    },
-    public productBankPrice?: number,
+    public basicPrice: number,
+    public planBonuses: Array<BonusDetails>,
+    public productBank: Array<ProductBankDetails>,
     public adquired?: boolean
   ) {}
 }
