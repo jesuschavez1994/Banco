9f53b0043e5837dd76bbafd49eddeea840a061c6
Javier Gomez <ashair455@gmail.com>
2021-02-03 17:45:36 -0430

    imgs slider add

--- src/app/shared/slider/slider.component.ts
+++ src/app/shared/slider/slider.component.ts
@@ -9,16 +9,16 @@ export class SliderComponent implements OnInit {
 
   constructor() { }
   item1: any[]=[
-    'assets/images/sliderHome/bannertipo1/1200por220.svg',
-    'assets/images/sliderHome/bannertipo1/768por300.jpg',
-    'assets/images/sliderHome/bannertipo1/425por300.jpg',
+    'assets/images/sliderHome/bannertipo1/1200x220.jpg',
+    'assets/images/sliderHome/bannertipo1/768x300.jpg',
+    'assets/images/sliderHome/bannertipo1/425x300.jpg',
     'assets/images/sliderHome/bannertipo1/325x300.jpg'
   ];
   item2: any[]=[
-    'assets/images/sliderHome/bannertipo2/1200x220.svg',
-    'assets/images/sliderHome/bannertipo2/768x300.svg',
-    'assets/images/sliderHome/bannertipo2/425x300.svg',
-    'assets/images/sliderHome/bannertipo2/325x300.svg'
+    'assets/images/sliderHome/bannertipo2/1200x220.jpg',
+    'assets/images/sliderHome/bannertipo2/768x300.jpg',
+    'assets/images/sliderHome/bannertipo2/425x300.jpg',
+    'assets/images/sliderHome/bannertipo2/325x300.jpg'
   ];
   ngOnInit(): void {
   }
