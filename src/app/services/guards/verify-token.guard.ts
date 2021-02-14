ac0ba1518002dc0d6a521be82be7bcf3ae19b055
chavez <rc620620@gmail.com>
2021-02-14 20:28:49 +0100

    Se agrego en el guard del token si ha expirado redireccione al Login

--- src/app/services/guards/verify-token.guard.ts
+++ src/app/services/guards/verify-token.guard.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { StoreService } from '../store/store.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerifyTokenGuard implements CanActivate {
 
-  constructor(public auth: StoreService){}
+  constructor(public auth: StoreService, public router: Router){}
 
   canActivate(): Promise<boolean> | boolean {
 
@@ -19,13 +20,15 @@ export class VerifyTokenGuard implements CanActivate {
     let expirado = this.expirado( payload.exp );
 
     if ( expirado ) {
+      this.router.navigate(['/login']);
+      console.log('Token Expirado');
       return false;
     }
 
     console.log('Payload', payload);
 
     return true;
-    
+
   }
 
   expirado( fechaExp: number ) {
