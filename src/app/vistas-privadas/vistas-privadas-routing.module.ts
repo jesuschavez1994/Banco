a38b536818b6e8d89b83422589fadc6297f9b559
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-14 17:11:58 -0400

    Organizacion de codigo.

--- src/app/vistas-privadas/vistas-privadas-routing.module.ts
+++ src/app/vistas-privadas/vistas-privadas-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ROUTES } from './vistas-privadas.routes';
-
 @NgModule({
   imports: [RouterModule.forChild(ROUTES)],
   exports: [RouterModule],
