6d545e9c2b6f8c333f709223d2c71831a5d44c57
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-14 10:07:13 -0400

    Importadas las rutas de las vistas públicas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
   All the app's routes
 */
 import { APP_ROUTES } from './app.routes';
+import { VistasPublicasRoutingModule } from './vistas-publicas/vistas-publicas-routing.module';
 
 @NgModule({
-  imports: [RouterModule.forRoot(APP_ROUTES, { useHash: true })],
+  imports: [
+    RouterModule.forRoot(APP_ROUTES, { useHash: true }),
+    VistasPublicasRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
