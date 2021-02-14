29856707032d21ff7c354d3f8af1f0ab466c324b
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-13 20:24:03 -0400

    Organización de código y creación de módulo para aplicar lazy loading.

--- src/app/vistas-publicas/vistas-publicas.module.ts
+++ src/app/vistas-publicas/vistas-publicas.module.ts
@@ -1,11 +1,11 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../vistas-publicas/home/home.component';
 import { RegisterComponent } from './Registers/Usuario/register.component';
 import { FormularioRegisterModule } from '../form-register/formulario-register.module';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
-import {PipesModule } from '@pipes/pipes.module';
+import { PipesModule } from '@pipes/pipes.module';
 import { NgxSpinnerModule } from 'ngx-spinner';
 // import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ToastComponent } from '../modals/toast/toast.component';
@@ -18,7 +18,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../Angula-Material/material.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { PrivateviewModule } from '../vistas-privadas/privateview.module';
-import { APP_ROUTING } from '../app.routes';
+// import { APP_ROUTING } from '../app.routes';
 import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
 import { SliderCategoryComponent } from './home/slider-category/slider-category.component';
 import { CategorysComponent } from './categorys/categorys.component';
@@ -26,43 +26,50 @@ import { MenuCategorysComponent } from './categorys/menu-categorys/menu-category
 import { ListProductComponent } from './categorys/list-product/list-product.component';
 import { ModalRecoverPasswordComponent } from './login/container/modal-recover-password/modal-recover-password.component';
 
+/* 
+  Components go here.
+*/
+const components = [
+  HomeComponent,
+  RegisterComponent,
+  BusinessDetailComponent,
+  // ToastComponent,
+  LoginComponent,
+  PageUnderConstructionComponent,
+  SliderCategoryComponent,
+  CategorysComponent,
+  MenuCategorysComponent,
+  ListProductComponent,
+  ModalRecoverPasswordComponent,
+];
+/* 
+  Modules go here.
+*/
+const modules = [
+  CommonModule,
+  MaterialModule,
+  AppRoutingModule,
+  FormularioRegisterModule,
+  PrivateviewModule,
+  ReactiveFormsModule,
+  FormsModule,
+  SharedModule,
+  // APP_ROUTING,
+  RouterModule,
+  ComponentsModule,
+  PanelModule,
+  PipesModule,
+  // ShoppingCartModule,
+  NgxSpinnerModule,
+];
+/* 
+  If need that this module exports something, put those modules/components here.
+*/
+const exportStuff = [HomeComponent, RegisterComponent];
 
 @NgModule({
-  declarations: [
-
-    HomeComponent,
-    RegisterComponent,
-    BusinessDetailComponent,
-    // ToastComponent,
-
-    LoginComponent,
-    PageUnderConstructionComponent,
-    SliderCategoryComponent,
-    CategorysComponent,
-    MenuCategorysComponent,
-    ListProductComponent,
-    ModalRecoverPasswordComponent,
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    AppRoutingModule,
-    FormularioRegisterModule,
-    PrivateviewModule,
-    ReactiveFormsModule,
-    FormsModule,
-    SharedModule,
-    APP_ROUTING,
-    RouterModule,
-    ComponentsModule,
-    PanelModule,
-    PipesModule,
-    // ShoppingCartModule,
-    NgxSpinnerModule,
-  ],
-  exports: [
-    HomeComponent,
-    RegisterComponent,
-  ]
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...exportStuff],
 })
-export class VistasPublicasModule { }
+export class VistasPublicasModule {}
