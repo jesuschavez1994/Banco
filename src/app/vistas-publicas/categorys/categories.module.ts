299f9e6824c9acaac6a601963dc26543cb43c7b7
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-14 12:33:52 -0400

    - Implemetacion de modulos dedicados para la caracteristica "categorys". - Implementacion de modulo de rutas para la caracteristica. - Importacion de modulos y componentes necesarios.

--- src/app/vistas-publicas/categorys/categories.module.ts
+++ src/app/vistas-publicas/categorys/categories.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+/* 
+  Modules go here
+*/
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { SharedModule } from '@app/shared/shared.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+/* 
+  Components go here
+*/
+import { CategorysComponent } from './categorys.component';
+import { MenuCategorysComponent } from './menu-categorys/menu-categorys.component';
+import { ListProductComponent } from './list-product/list-product.component';
 
+const components = [
+  CategorysComponent,
+  MenuCategorysComponent,
+  ListProductComponent,
+];
+
+const modules = [CategoriesRoutingModule, NgxSpinnerModule, SharedModule];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ]
+  declarations: [...components],
+  imports: [...modules],
 })
-export class CategoriesModule { }
+export class CategoriesModule {}
