70b514495aec6c354d23ac97f6480222acb609a3
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-14 10:13:11 -0400

    Implementacion del nuevo modulo de rutas.

--- src/app/vistas-publicas/vistas-publicas.module.ts
+++ src/app/vistas-publicas/vistas-publicas.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from '../vistas-publicas/home/home.component';
 import { RegisterComponent } from './Registers/Usuario/register.component';
 import { FormularioRegisterModule } from '../form-register/formulario-register.module';
@@ -10,15 +11,12 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 // import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ToastComponent } from '../modals/toast/toast.component';
 import { BusinessDetailComponent } from './business-detail/business-detail.component';
-import { RouterModule } from '@angular/router';
 import { PanelModule } from './panel/panel.module';
 
 import { LoginComponent } from './login/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../Angula-Material/material.module';
-import { AppRoutingModule } from '../app-routing.module';
 import { PrivateviewModule } from '../vistas-privadas/privateview.module';
-// import { APP_ROUTING } from '../app.routes';
 import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
 import { SliderCategoryComponent } from './home/slider-category/slider-category.component';
 import { CategorysComponent } from './categorys/categorys.component';
@@ -55,12 +53,12 @@ const modules = [
   FormsModule,
   SharedModule,
   // APP_ROUTING,
-  RouterModule,
   ComponentsModule,
   PanelModule,
   PipesModule,
   // ShoppingCartModule,
   NgxSpinnerModule,
+  VistasPublicasRoutingModule,
 ];
 /* 
   If need that this module exports something, put those modules/components here.
