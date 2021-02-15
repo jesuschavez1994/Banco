956240ded354b9b85adc58da1b5ac083318ec82d
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-15 16:26:22 -0400

    Copiados los archivos de la implementacion original para recrearla.

---
+++ src/app/vistas-privadas/my-store/components/cards-shimmer/card-shimmer-title-store/card-shimmer-title-store.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-shimmer-title-store',
+  templateUrl: './card-shimmer-title-store.component.html',
+  styleUrls: ['./card-shimmer-title-store.component.scss']
+})
+export class CardShimmerTitleStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/vistas-privadas/my-store/components/schedule-store/schedule-store.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef  } from '@angular/core';
+import { StoreService } from '@services/store/store.service';
+
+@Component({
+  selector: 'app-schedule-store',
+  templateUrl: './schedule-store.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./schedule-store.component.css']
+})
+export class ScheduleStoreComponent implements OnInit {
+
+  Day = [
+    {
+      dia: 'Lunes',
+    },
+    {
+      dia: 'Martes',
+    },
+    {
+      dia: 'Miercoles',
+    },
+    {
+      dia: 'Jueves',
+    },
+    {
+      dia: 'Viernes',
+    },
+    {
+      dia: 'Sabado',
+    },
+    {
+      dia: 'Domingo',
+    }
+  ];
+
+  schedules: any[] = [];
+
+  // tslint:disable-next-line: variable-name
+  constructor(public _storeService: StoreService, private changeDetector: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this._storeService.GetShedule(
+      localStorage.getItem('id'),
+      localStorage.getItem('storeId'))
+      .subscribe( response => {
+        console.log('GET', response);
+        this.schedules.push(response);
+        console.log('Horario', this.schedules);
+        this.changeDetector.markForCheck();
+    });
+  }
+
+}
