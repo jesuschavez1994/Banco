9433378c5f4b7c976faf6f09eaa1cfccfc026b83
chavez <rc620620@gmail.com>
2021-02-15 11:53:28 -0400

    Se hicieron mejoras en cuanto al Orden de los Modulos de los Pipes, Shared, Services y Vistas-Publicas => Las mejoras conciste en que se creó un archivo index el cuál posee todas la importaciones ya sea un index para Components y Otro index para las importaciones de los Modulos => Con esto evitamos hacer multiples importaciones en un Modulo colocando así en el Modulo pricipal una sola importacion que contenga todos los componentes => import {index-que-contienes-todos-los-componentes} from './index.ts' => EL PUSH SE HACE A LA RAMA DE integration-javier-christopher

---
+++ src/app/vistas-publicas/vista-publicas.index.ts
@@ -0,0 +1,8 @@
+export { ToastComponent } from '../modals/toast/toast.component';
+export { BusinessDetailComponent } from './business-detail/business-detail.component';
+export { HomeComponent } from '../vistas-publicas/home/home.component';
+export { RegisterComponent } from './Registers/Usuario/register.component';
+export { LoginComponent } from './login/login/login.component';
+export { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
+export { SliderCategoryComponent } from './home/slider-category/slider-category.component';
+export { ModalRecoverPasswordComponent } from './login/container/modal-recover-password/modal-recover-password.component';
