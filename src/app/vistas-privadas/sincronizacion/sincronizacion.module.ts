6a9f1126c54b750aeeb4e0dd89d4fb1d6e56a081
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-15 10:48:35 -0400

    [Lazy loading funcional] Importaciones de los modulos y componentes necesarios para la funcionalidad de la vista.

--- src/app/vistas-privadas/sincronizacion/sincronizacion.module.ts
+++ src/app/vistas-privadas/sincronizacion/sincronizacion.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 
 import { SincronizacionRoutingModule } from './sincronizacion-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { VistasPrivadasSharedModule } from '../shared/vistas-privadas-shared.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SuggestedProductsModule } from './pages/suggested-products/suggested-products.module';
 
 import { BankProductComponent } from './pages/bank-product/bank-product.component';
+import { DesincronizarComponent } from './components/desincronizar/desincronizar.component';
+import { EditSincronizacionComponent } from './components/edit-sincronizacion/edit-sincronizacion.component';
 import { ExportarListaExcelComponent } from './pages/exportar-lista-excel/exportar-lista-excel.component';
+import { FormBanckProductSyncComponent } from './components/form-banck-product-sync/form-banck-product-sync.component';
+import { SearchModule } from './components/search/search.module';
 import { SuggestedProductsComponent } from './pages/suggested-products/suggested-products.component';
 import { SincronizacionViewsComponent } from './components/sincronizacion-views/sincronizacion-views.component';
 import { SynchronizedProductsComponent } from './pages/synchronized-products/synchronized-products.component';
@@ -13,15 +20,24 @@ import { SynchronizedProductsComponent } from './pages/synchronized-products/syn
 */
 const components = [
   BankProductComponent,
+  DesincronizarComponent,
+  EditSincronizacionComponent,
   ExportarListaExcelComponent,
-  SuggestedProductsComponent,
+  FormBanckProductSyncComponent,
   SincronizacionViewsComponent,
   SynchronizedProductsComponent,
 ];
 /* 
   Modules go here.
 */
-const modules = [SincronizacionRoutingModule, SharedModule];
+const modules = [
+  NgxSpinnerModule,
+  SincronizacionRoutingModule,
+  SearchModule,
+  SharedModule,
+  SuggestedProductsModule,
+  VistasPrivadasSharedModule,
+];
 /* 
   If need that this module exports something, put those modules/components here.
 */
