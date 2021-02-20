cb84755cc9518229e52fea30634272c35ff6f995
jacintoaczz <jacintoaczz@users.noreply.github.com>
2021-02-19 22:20:20 -0400

    [ Trabajando en ello ] Creacion de servicio para sincronizar productos en masa.

--- src/app/services/sincronizacion/sincronizacion.service.ts
+++ src/app/services/sincronizacion/sincronizacion.service.ts
@@ -1,145 +1,165 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { URL_SERVICIOS } from '../../config/config';
-import * as XLSX from 'xlsx';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { URL_SERVICIOS } from '../../config/config'
+import * as XLSX from 'xlsx'
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class SincronizacionService {
+  constructor(public http: HttpClient) {}
 
-  constructor(public http: HttpClient) { }
-
-  private postQuery<T>(query: string, data: any){
-
+  private postQuery<T>(query: string, data: any) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-    });
+    })
 
-
-    query = URL_SERVICIOS + query;
-    return this.http.post<T>( query, data, {headers} );
+    query = URL_SERVICIOS + query
+    return this.http.post<T>(query, data, { headers })
   }
 
-  private execQuery<T>( query: string ) {
-    query = URL_SERVICIOS + query;
-    return this.http.get<T>( query );
+  private execQuery<T>(query: string) {
+    query = URL_SERVICIOS + query
+    return this.http.get<T>(query)
   }
 
-  private DeleteQuery<T>( query: string ) {
-    query = URL_SERVICIOS + query;
-    return this.http.delete<T>( query );
+  private DeleteQuery<T>(query: string) {
+    query = URL_SERVICIOS + query
+    return this.http.delete<T>(query)
   }
 
-  GetAllProductSuggested(userId: string, storeId: string, page?: number){
-    const url = `/api/users/${userId}/stores/${storeId}/product_suggesteds`  + '?page=' + page;
-    return this.execQuery(url);
+  GetAllProductSuggested(userId: string, storeId: string, page?: number) {
+    const url =
+      `/api/users/${userId}/stores/${storeId}/product_suggesteds` +
+      '?page=' +
+      page
+    return this.execQuery(url)
   }
 
   // /api/users/2/stores/1/productcsv
 
-  PostListadoProductosExcel(userId: string, storeId: string, file: any){
-    const url = `/api/users/${userId}/stores/${storeId}/productcsv`;
-    return this.postQuery(url, file);
+  PostListadoProductosExcel(userId: string, storeId: string, file: any) {
+    const url = `/api/users/${userId}/stores/${storeId}/productcsv`
+    return this.postQuery(url, file)
   }
 
   // http://192.168.0.130/founduss/public/api/users/1/stores/1/products/2/syncs
 
-  productSyncrhonized(userId: string, storeId: string, IdProduct: string, post: any){
-    const url = `/api/users/${userId}/stores/${storeId}/products/${IdProduct}/syncs`;
-    return this.postQuery(url, post);
+  productSyncrhonized(
+    userId: string,
+    storeId: string,
+    IdProduct: string,
+    post: any
+  ) {
+    const url = `/api/users/${userId}/stores/${storeId}/products/${IdProduct}/syncs`
+    return this.postQuery(url, post)
   }
 
-  DowloadFileExcelListProduct(userId: string, storeId: string){
-    const url = URL_SERVICIOS + `/api/users/${userId}/stores/${storeId}/dowload_productcsv`;
-    return this.http.get(url, {responseType: 'blob'});
+  DowloadFileExcelListProduct(userId: string, storeId: string) {
+    const url =
+      URL_SERVICIOS +
+      `/api/users/${userId}/stores/${storeId}/dowload_productcsv`
+    return this.http.get(url, { responseType: 'blob' })
   }
 
-  GetBankProduct(page?: number){
-    const url = `/api/banks` + '?page=' + page;
-    return this.execQuery(url);
+  GetBankProduct(page?: number) {
+    const url = `/api/banks` + '?page=' + page
+    return this.execQuery(url)
   }
 
-  GetBankProductSpecific(idBankSync: string){
-    const url = `/api/banks/${idBankSync}`;
-    return this.execQuery(url);
+  GetBankProductSpecific(idBankSync: string) {
+    const url = `/api/banks/${idBankSync}`
+    return this.execQuery(url)
   }
 
-  SincronizarDesdeBancoPrdoducto(userId: string, storeId: string, data){
-    const url = `/api/users/${userId}/stores/${storeId}/sync_create`;
-    return this.postQuery(url, data);
+  SincronizarDesdeBancoPrdoducto(userId: string, storeId: string, data) {
+    const url = `/api/users/${userId}/stores/${storeId}/sync_create`
+    return this.postQuery(url, data)
   }
 
-  ListProductSincronizadosYNosincronizados(userId: string, storeId: string, page?: any){
-    const url = `/api/users/${userId}/stores/${storeId}/products_syncs` + '?page=' + page;
-    return this.execQuery(url);
+  bulkProductsSync(userId: string, storeId: string, payload) {
+    const url = `/api/users/${userId}/stores/${storeId}/syncs`
+    return this.postQuery(url, payload)
   }
 
-
-  Sugerir(storeId: string, id: any){
-    const url = `/stores/${storeId}/suggests`;
-    return this.postQuery(url, id);
+  ListProductSincronizadosYNosincronizados(
+    userId: string,
+    storeId: string,
+    page?: any
+  ) {
+    const url =
+      `/api/users/${userId}/stores/${storeId}/products_syncs` + '?page=' + page
+    return this.execQuery(url)
   }
 
-  BuscadorSugerencias(termino: any, userId: string, storeId: string){
-    const url = `/api/users/${userId}/stores/${storeId}/products_suggested_search`;
-    return this.postQuery(url, termino);
+  Sugerir(storeId: string, id: any) {
+    const url = `/stores/${storeId}/suggests`
+    return this.postQuery(url, id)
   }
 
-  BuscadorBancoDeProductos(termino: any, userId: string, storeId: string){
-    const url = `/api/users/${userId}/stores/${storeId}/banks_search`;
-    return this.postQuery(url, termino);
+  BuscadorSugerencias(termino: any, userId: string, storeId: string) {
+    const url = `/api/users/${userId}/stores/${storeId}/products_suggested_search`
+    return this.postQuery(url, termino)
   }
 
-  Desincronizar(userId: string, storeId: string, productId: string, idSync: string){
-    const url = `/api/users/${userId}/stores/${storeId}/products/${productId}/syncs/${idSync}`;
-    return this.DeleteQuery(url);
+  BuscadorBancoDeProductos(termino: any, userId: string, storeId: string) {
+    const url = `/api/users/${userId}/stores/${storeId}/banks_search`
+    return this.postQuery(url, termino)
+  }
+
+  Desincronizar(
+    userId: string,
+    storeId: string,
+    productId: string,
+    idSync: string
+  ) {
+    const url = `/api/users/${userId}/stores/${storeId}/products/${productId}/syncs/${idSync}`
+    return this.DeleteQuery(url)
   }
 
   public ShowTableExcell(bstr: string): XLSX.AOA2SheetOpts {
     /* read workbook */
-    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' })
 
     /* grab first sheet */
-    const wsname: string = wb.SheetNames[0];
-    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+    const wsname: string = wb.SheetNames[0]
+    const ws: XLSX.WorkSheet = wb.Sheets[wsname]
 
     /* save data */
-    const data = (XLSX.utils.sheet_to_json(ws, { header: 1 })) as XLSX.AOA2SheetOpts;
+    const data = XLSX.utils.sheet_to_json(ws, {
+      header: 1,
+    }) as XLSX.AOA2SheetOpts
 
-    return data;
+    return data
   }
-
 
   public importFromFile(bstr: string): XLSX.AOA2SheetOpts {
     /* read workbook */
-    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'base64' });
+    const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'base64' })
 
     /* grab first sheet */
-    const wsname: string = wb.SheetNames[0];
-    const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+    const wsname: string = wb.SheetNames[0]
+    const ws: XLSX.WorkSheet = wb.Sheets[wsname]
 
     /* save data */
-    const data = (XLSX.utils.sheet_to_json(ws, { header: 1 })) as XLSX.AOA2SheetOpts;
+    const data = XLSX.utils.sheet_to_json(ws, {
+      header: 1,
+    }) as XLSX.AOA2SheetOpts
 
-    return data;
+    return data
   }
-
 
   // tslint:disable-next-line: variable-name
   public exportToFile(fileName: string, element_id: string) {
-    if (!element_id){
-      throw new Error('Element Id does not exists');
+    if (!element_id) {
+      throw new Error('Element Id does not exists')
     }
-    const tbl = document.getElementById(element_id);
-    const wb = XLSX.utils.table_to_book(tbl);
-    XLSX.writeFile(wb, fileName + '.xlsx');
+    const tbl = document.getElementById(element_id)
+    const wb = XLSX.utils.table_to_book(tbl)
+    XLSX.writeFile(wb, fileName + '.xlsx')
   }
-
 }
