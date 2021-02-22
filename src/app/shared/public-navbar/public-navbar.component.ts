fd86ec3b8a676f76a4deb99127e7a0eea72cbb94
Jacinto Acosta <jacintoaczz@gmail.com>
2021-02-22 09:03:26 -0400

    [ Trabajo en progreso ] - Trabajando en la barra de busqueda y los metodos para manejar la busqueda.

--- src/app/shared/public-navbar/public-navbar.component.ts
+++ src/app/shared/public-navbar/public-navbar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, Input , ViewChild} from '@angular/core';
-import { DropdownOption, ClassIcon, ExtraButtonEmitter } from '@interfaces/components-options/dropdown.options.interface';
-import { HomeServiceService } from "../../vistas-publicas/services/home-service.service";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  SimpleChange,
+} from '@angular/core'
+import {
+  DropdownOption,
+  ClassIcon,
+  ExtraButtonEmitter,
+} from '@interfaces/components-options/dropdown.options.interface'
+import { SearchService } from '@services/Search/search.service'
+import { HomeServiceService } from '../../vistas-publicas/services/home-service.service'
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-public-navbar',
   templateUrl: './public-navbar.component.html',
-  styleUrls: ['./public-navbar.component.scss']
+  styleUrls: ['./public-navbar.component.scss'],
 })
 export class PublicNavbarComponent implements OnInit {
-  @Input() userLog: boolean;
-  @Input() storeAct: boolean | string;
+  @Input() userLog: boolean
+  @Input() storeAct: boolean | string
 
   // Button DropDown - cart
   classIcon: ClassIcon = {
@@ -18,9 +30,9 @@ export class PublicNavbarComponent implements OnInit {
     extraButton: {
       name: 'delete',
       class: 'fas fa-trash',
-      color: '#f32323'
-    }
-  };
+      color: '#f32323',
+    },
+  }
   // Button DropDown - favorite
   classIconFavorite: ClassIcon = {
     class: 'fas fa-heart',
@@ -28,16 +40,49 @@ export class PublicNavbarComponent implements OnInit {
     extraButton: {
       name: 'delete',
       class: 'fas fa-trash',
-      color: '#f32323'
-    }
-  };
-  @Input() menuOptions: DropdownOption[] = [];
-  @Input() menuOptionsFavorite: DropdownOption[] = [];
+      color: '#f32323',
+    },
+  }
+  @Input() menuOptions: DropdownOption[] = []
+  @Input() menuOptionsFavorite: DropdownOption[] = []
 
-  constructor(public homeService: HomeServiceService,) { }
+  constructor(
+    public homeService: HomeServiceService,
+    public _searchService: SearchService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.userLog = this.homeService.islog();
-     this.storeAct= this.homeService.storeActive();
+    this.userLog = this.homeService.islog()
+    this.storeAct = this.homeService.storeActive()
+  }
+
+  // Handlers for events that happen in the component ----------------------
+  handleSearch(searchTerm: string) {
+    let globalSearchPayload = {
+      name: searchTerm,
+    }
+
+    this._searchService
+      .globalProductSearch(globalSearchPayload)
+      .subscribe((productsData) => {
+        console.log('Global product search data: ')
+        console.log(productsData)
+
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: searchTerm !== '' ? { name: searchTerm } : {},
+        })
+      })
+  }
+
+  public search(ToSearch) {
+    console.log(ToSearch.value)
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: ToSearch.value !== '' ? { name: ToSearch.value } : {},
+    })
   }
 }
