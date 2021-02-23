1970e19ceeabbd26aa34f01de4fd82fac8257175
Jacinto Acosta <jacintoaczz@gmail.com>
2021-02-23 11:01:59 -0400

    - Formateo con Prettier.

--- src/app/shared/products-cards/products-cards.component.ts
+++ src/app/shared/products-cards/products-cards.component.ts
@@ -1,46 +1,44 @@
-import { Component, Input, OnInit, EventEmitter, Output, ViewChild } from '@angular/core';
-import { ProductsCardsOptions } from '@interfaces/components-options/products-cards.option.interface';
-import { Product } from '@interfaces/product.interface';
+import {
+  Component,
+  Input,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core'
+import { ProductsCardsOptions } from '@interfaces/components-options/products-cards.option.interface'
+import { Product } from '@interfaces/product.interface'
 
 @Component({
   selector: 'app-products-cards',
   templateUrl: './products-cards.component.html',
-  styleUrls: ['./products-cards.component.scss']
+  styleUrls: ['./products-cards.component.scss'],
 })
 export class ProductsCardsComponent implements OnInit {
+  p = 1
+  @Input() itemsPerPage = 16
+  @Input() totalItems: number
+  @Input() products: ProductsCardsOptions[] = []
+  @Input() showShimmer = true
 
-  p = 1;
-  @Input() itemsPerPage = 16;
-  @Input() totalItems: number;
-  @Input() products: ProductsCardsOptions[] = [];
-  @Input() showShimmer = true;
+  @Output() selected = new EventEmitter<ProductsCardsOptions>()
+  @Output() pagination = new EventEmitter<number>()
 
-  @Output() selected = new EventEmitter<ProductsCardsOptions>();
-  @Output() pagination = new EventEmitter<number>();
+  constructor() {}
 
+  ngOnInit(): void {}
 
-
-  constructor(
-
-
-  ) { }
-
-  ngOnInit(): void {
+  public selectedProduct(product: ProductsCardsOptions) {
+    this.selected.emit(product)
   }
-
-  public selectedProduct(product: ProductsCardsOptions){
-    this.selected.emit(product);
-  }
-
 
   public pageChanged(event) {
-    this.p = event;
+    this.p = event
 
-    this.pagination.emit(this.p);
-
+    this.pagination.emit(this.p)
   }
 
   public toggleShimmer(show = true) {
-    this.showShimmer = show;
+    this.showShimmer = show
   }
 }
