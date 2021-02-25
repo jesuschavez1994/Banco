060bf8fbe44046ad00c3b01db8f4fd75eecc435f
Jacinto Acosta <jacintoaczz@gmail.com>
2021-02-25 10:36:10 -0400

    - Añadidos titulos a las rutas. - Rama: fix/sync-this-product

--- src/app/vistas-privadas/my-store/child-routes.module.ts
+++ src/app/vistas-privadas/my-store/child-routes.module.ts
@@ -1,25 +1,30 @@
-import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { ContactComponent } from './pages/contact/contact.component'
-import { ProductLoadingComponent } from '../product-loading/product-loading.component'
-import { FormBanckProductSyncComponent } from './pages/sincronizacion/components/form-banck-product-sync/form-banck-product-sync.component'
-import { EditProdutcComponent } from '../components/edit-produtc/edit-produtc.component'
-import { EditSincronizacionComponent } from './pages/sincronizacion/components/edit-sincronizacion/edit-sincronizacion.component'
-import { EditProductNoDisponibleComponent } from '../components/edit-product-no-disponible/edit-product-no-disponible.component'
-import { DesincronizarComponent } from './pages/sincronizacion/components/desincronizar/desincronizar.component'
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductLoadingComponent } from '../product-loading/product-loading.component';
+import { FormBanckProductSyncComponent } from './pages/sincronizacion/components/form-banck-product-sync/form-banck-product-sync.component';
+import { EditProdutcComponent } from '../components/edit-produtc/edit-produtc.component';
+import { EditSincronizacionComponent } from './pages/sincronizacion/components/edit-sincronizacion/edit-sincronizacion.component';
+import { EditProductNoDisponibleComponent } from '../components/edit-product-no-disponible/edit-product-no-disponible.component';
+import { DesincronizarComponent } from './pages/sincronizacion/components/desincronizar/desincronizar.component';
 
 const childRoutes: Routes = [
   {
     path: 'contact',
     component: ContactComponent,
   },
-  { path: 'product-catalogue/:idStore',  // Se obtiene el id de la tienda para mostrar su listo productos
-loadChildren: () => import('./pages/load-product/load-product.module').then(module => module.LoadProductModule)
- },
+  {
+    path: 'product-catalogue/:idStore', // Se obtiene el id de la tienda para mostrar su listo productos
+    loadChildren: () =>
+      import('./pages/load-product/load-product.module').then(
+        (module) => module.LoadProductModule
+      ),
+  },
   {
     path: 'load-product',
     component: ProductLoadingComponent,
+    data: { title: 'Cargar producto | Founduss' },
   },
   {
     path: 'sync-this-product/:id',
@@ -28,6 +33,7 @@ loadChildren: () => import('./pages/load-product/load-product.module').then(modu
   {
     path: 'edit-product-sync/:id',
     component: EditSincronizacionComponent,
+    data: { title: 'Sincronizar producto | Founduss' },
   },
   {
     path: 'desincronizar/:id',
@@ -50,7 +56,7 @@ loadChildren: () => import('./pages/load-product/load-product.module').then(modu
   },
 
   { path: '**', pathMatch: 'full', redirectTo: 'contact' },
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(childRoutes)],
