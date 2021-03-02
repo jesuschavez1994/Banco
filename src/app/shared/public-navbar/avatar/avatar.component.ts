bc6a0ccf758e397c446c6817405c1daaeb071c8e
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-02 08:56:40 -0400

    - Creacion e implementacion de servicio para actualizar la data de la imagen. - Creada suscripcion al observable del servicio.

--- src/app/shared/public-navbar/avatar/avatar.component.ts
+++ src/app/shared/public-navbar/avatar/avatar.component.ts
@@ -1,48 +1,68 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { StoreService } from '@services/store/store.service';
-import { HomeServiceService } from "../../../vistas-publicas/services/home-service.service";
+import { HomeServiceService } from '../../../vistas-publicas/services/home-service.service';
 import { environment } from '@environments/environment';
+
+import { AvatarService } from '@shared/public-navbar/avatar/services/avatar.service';
+import { Image } from '@interfaces/userPublic.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.scss']
+  styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent implements OnInit {
+export class AvatarComponent implements OnInit, OnDestroy {
   userId: number | string;
   userImg: any;
   userName: string;
   userEmail: string;
-  envApi= environment.url;
+  envApi = environment.url;
 
   @Input() imgCropper: any;
 
   storeAct: boolean | string = false;
-  constructor(private auth: StoreService, public homeService: HomeServiceService,) {
+  subscription: Subscription;
+
+  constructor(
+    private auth: StoreService,
+    public homeService: HomeServiceService,
+    private _avatarService: AvatarService
+  ) {
     this.imgUser();
-    
+    this.subscription = _avatarService.imageData$.subscribe(
+      (imageData: Image[]) => {
+        console.log('User image data:');
+        console.log(imageData);
+        this.userImg = imageData;
+      }
+    );
   }
 
   ngOnInit(): void {
-    this.storeAct= this.homeService.storeActive();
+    this.storeAct = this.homeService.storeActive();
     console.log('storeAct', this.storeAct);
   }
-  imgUser(){
-    if(this.auth){
-      this.userId= localStorage.getItem('id');
-      this.homeService.obtUserData(this.userId).subscribe(
-        data=>{
-          
-           this.userImg=data.image;
-           this.userEmail= data.email;
-           this.userName= data.name;
-          console.log(data);
-        }
-      );
+
+  ngOnDestroy(): void {
+    // Preventing memory leaks
+    this.subscription.unsubscribe();
+  }
+
+  imgUser() {
+    if (this.auth) {
+      this.userId = localStorage.getItem('id');
+      this.homeService.obtUserData(this.userId).subscribe((data) => {
+        // this.userImg = data.image; // Testing the service
+        this._avatarService.setImageData(data.image);
+        this.userEmail = data.email;
+        this.userName = data.name;
+        console.log(data);
+      });
     }
-  } 
-  logout(){
+  }
+  logout() {
     this.auth.logout();
-   // window.location.reload();
+    // window.location.reload();
   }
 }
