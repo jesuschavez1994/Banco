18bf511db03aa90a5ef9edcb5ecb6f0c652670d0
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-03 09:52:27 -0400

    - Creacion e implementacion de servicio para actualizar la imagen del banner. - [ Funcional ] Carga la imagen del banner en ruta /my-store correctamente. - TODO: Actualizar la imagen con la del cropper.
    -> Rama: feature/banner-edit

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,20 +1,41 @@
-import { Component, HostListener, Input, OnInit, OnChanges } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  OnInit,
+  OnChanges,
+} from '@angular/core';
 import { BannerOptions } from '@interfaces/components-options/banner.options.interface';
+
+import { BannerService } from '@shared/banner/services/banner.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 // export class BannerComponent implements OnInit, OnChanges {
 export class BannerComponent implements OnInit, OnChanges {
-
   @Input() imgs: BannerOptions;
 
   currentImg: string;
   widthWindow = 480;
 
-  constructor() { }
+  images: any;
+  bannerSubscription: Subscription;
+  constructor(private _bannerService: BannerService) {
+    this.bannerSubscription = _bannerService.bannerImageData$.subscribe(
+      (bannerData: any) => {
+        console.log('Banner data observable on banner.component:');
+        console.log(bannerData);
+        // this.images = bannerData;
+        // this.currentImg = this.images.xl;
+        // console.log('Current banner images:');
+        // console.log(this.images);
+      }
+    );
+  }
 
   ngOnInit(): void {
     if (this.imgs) {
@@ -23,7 +44,6 @@ export class BannerComponent implements OnInit, OnChanges {
 
     // console.log('ngOnInit - imgs');
     // console.log(this.imgs);
-
   }
 
   ngOnChanges() {
@@ -32,29 +52,23 @@ export class BannerComponent implements OnInit, OnChanges {
     // console.log(this.currentImg);
 
     this.loadCurrentImg();
-
   }
 
   @HostListener('window:resize', ['$event'])
-  public changeImg( $event: Event){
+  public changeImg($event: Event) {
     this.widthWindow = window.innerWidth;
 
     this.loadCurrentImg();
-
   }
 
   public loadCurrentImg() {
     if (this.imgs) {
-
-      if ( this.widthWindow > 480 ) {
+      if (this.widthWindow > 480) {
         this.currentImg = this.imgs.m;
-
-      }else if ( this.widthWindow <= 480 ) {
-
-        if (this.imgs.s !== ''){
+      } else if (this.widthWindow <= 480) {
+        if (this.imgs.s !== '') {
           this.currentImg = this.imgs.s;
         }
-
       }
     }
   }
---
+++ src/app/shared/banner/services/banner.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { BannerImage, Srcsize } from '@interfaces/store.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BannerService {
+  // Observable data sources
+  private bannerImageDataSource = new Subject<
+    BannerImage[] | Srcsize | string
+  >();
+  private bannerEditImageDataSource = new Subject<BannerImage[] | string>();
+
+  // Observable data streams
+  bannerImageData$ = this.bannerImageDataSource.asObservable();
+  bannerEditImageData$ = this.bannerEditImageDataSource.asObservable();
+
+  // Service commands
+  setBannerImage(bannerImages: BannerImage[] | Srcsize | string) {
+    console.log('Data passed from params: ');
+    console.log(bannerImages);
+    this.bannerImageDataSource.next(bannerImages);
+  }
+}
