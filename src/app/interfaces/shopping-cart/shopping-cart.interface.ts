73b405fb4bfc94d0341635b3fd5e71d9808ba757
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-17 08:30:39 -0400

    - Creadas interfaces para organizacion de codigo. - Creada peticion para obtener todas las ordenes creadas por un usuario.

--- src/app/interfaces/shopping-cart/shopping-cart.interface.ts
+++ src/app/interfaces/shopping-cart/shopping-cart.interface.ts
@@ -57,3 +57,66 @@ export interface MallTransactionCredentials {
   token: string;
   url: string;
 }
+
+export interface OrderDetails {
+  id: number;
+  user_id: number;
+  title: string;
+  cart_data: string;
+  total_price: number;
+  currency: string;
+  status: string;
+  type: string;
+  created_at: string;
+  updated_at: string;
+  order_cart_items: OrderCartItem[];
+}
+
+interface OrderCartItem {
+  id: number;
+  order_id: number;
+  product_id: number;
+  price: number;
+  quantity: number;
+  created_at: string;
+  updated_at: string;
+  product: Product;
+}
+
+interface Product {
+  id: number;
+  store_id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  sincronice: string;
+  aviable: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: null;
+  store: Store;
+}
+
+interface Store {
+  id: number;
+  user_id: number;
+  rut: string;
+  name: string;
+  social_reason: string;
+  description: string;
+  level: string;
+  certification: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: null;
+  mall: Mall;
+}
+
+interface Mall {
+  id: number;
+  store_id: number;
+  commerce_code: string;
+  created_at: null;
+  updated_at: null;
+}
