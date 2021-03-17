ef1916b5c19cec5707af9a232f72a7fbe2555641
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-16 20:12:31 -0400

    - Modificado el HTML y la propiedad 'data' para dejar funcional el modal.

--- src/app/modals/confirm-webpay-plus/confirm-webpay-plus.component.ts
+++ src/app/modals/confirm-webpay-plus/confirm-webpay-plus.component.ts
@@ -1,55 +1,57 @@
 import { Component, OnInit, Inject, EventEmitter } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+interface ModalContent {
+  title: string;
+  mallTransaction: Credentials;
+}
+
+interface Credentials {
+  url: string;
+  token: string;
+}
+
 @Component({
   selector: 'app-confirm-webpay-plus',
   templateUrl: './confirm-webpay-plus.component.html',
-  styleUrls: ['./confirm-webpay-plus.component.scss']
+  styleUrls: ['./confirm-webpay-plus.component.scss'],
 })
 export class ConfirmWebpayPlusComponent implements OnInit {
-
   food: string;
   selectedData: any;
   buttonDisabled = false;
   emitSelectData = new EventEmitter();
-
+  dialogData: ModalContent;
 
   constructor(
     public dialogRef: MatDialogRef<ConfirmWebpayPlusComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: ModalContent
+  ) {
+    // We pass the data to the dialog context.
+    this.dialogData = {
+      title: data.title,
+      mallTransaction: {
+        url: data.mallTransaction.url,
+        token: data.mallTransaction.token,
+      },
+    };
 
-  ngOnInit(): void {
+    console.log('Dialog data');
+    console.log(this.dialogData);
   }
 
-  onNoClick(): void {
+  ngOnInit(): void {}
 
+  onNoClick(): void {
     this.emitSelectData.emit(this.selectedData);
 
     this.dialogRef.close({
-      food: this.food
+      food: this.food,
     });
   }
-
 
   onClose(specificData?) {
     const data = specificData ? specificData : this.data;
     this.dialogRef.close(data);
   }
-
-  onButton(specificData, close = true) {
-
-    this.selectedData = specificData;
-
-    this.emitSelectData.emit(this.selectedData);
-
-    console.log('onButton');
-    console.log(this.data.mallTransaction);
-
-    if (close) {
-      this.onClose(this.selectedData);
-    }
-
-  }
-
 }
