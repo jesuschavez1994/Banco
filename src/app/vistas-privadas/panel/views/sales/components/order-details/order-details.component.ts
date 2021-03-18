07430ba87d6cefe3b423f00b97c1e1c60e0d5826
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-18 19:02:24 -0400

    - Estructura general completada. -> TODO: Calcular el porcentaje total agregando el impuesto.

--- src/app/vistas-privadas/panel/views/sales/components/order-details/order-details.component.ts
+++ src/app/vistas-privadas/panel/views/sales/components/order-details/order-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Orders } from '@services/store-sales/store-sales.service';
+import { Orders, Datum } from '@services/store-sales/store-sales.service';
 
 @Component({
   selector: 'app-order-details',
@@ -7,7 +7,7 @@ import { Orders } from '@services/store-sales/store-sales.service';
   styleUrls: ['./order-details.component.scss'],
 })
 export class OrderDetailsComponent implements OnInit {
-  @Input() orderDetails: Orders;
+  @Input() orderDetails: Datum;
 
   constructor() {}
 
