058ba2e5b61c0ebedfcab648515826e713a7f591
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-18 19:03:18 -0400

    - Actualizada la interfaz utilizada en las ordenes.

--- src/app/vistas-privadas/panel/views/sales/views/sales-details/sales-details.component.ts
+++ src/app/vistas-privadas/panel/views/sales/views/sales-details/sales-details.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  StoreSalesService,
+  Orders,
+  Datum,
+} from '@services/store-sales/store-sales.service';
 
 @Component({
   selector: 'app-sales-details',
   templateUrl: './sales-details.component.html',
-  styleUrls: ['./sales-details.component.scss']
+  styleUrls: ['./sales-details.component.scss'],
 })
 export class SalesDetailsComponent implements OnInit {
+  storeOrders: Datum;
+  selectedOrderDetails: Orders;
+  loading = true;
 
-  constructor() { }
+  constructor(private _storeSalesService: StoreSalesService) {}
 
   ngOnInit(): void {
+    this._storeSalesService.getStoreOrders().subscribe((allOrders: Orders) => {
+      console.log('All the store orders');
+      console.log(allOrders);
+      this.storeOrders = allOrders.data;
+
+      this.selectedOrderDetails = this.storeOrders[0];
+      this.loading = false;
+    });
   }
 
+  public getOrderDetails(orderDetails: Orders) {
+    this.selectedOrderDetails = orderDetails;
+  }
 }
