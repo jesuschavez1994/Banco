5ae43a3ad816b246facc93c0fad75d4a049275a2
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-18 08:17:49 -0400

    - Importacion de nuevos componentes.

--- src/app/vistas-privadas/panel/views/sales/sales.module.ts
+++ src/app/vistas-privadas/panel/views/sales/sales.module.ts
@@ -5,13 +5,17 @@ import { SalesRoutingModule } from './sales-routing.module';
 import { SalesComponent } from './sales.component';
 import { SalesDetailsComponent } from './views/sales-details/sales-details.component';
 import { DeliveriesComponent } from './views/deliveries/deliveries.component';
+import { OrdersListComponent } from './components/orders-list/orders-list.component';
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { SkeletonListComponent } from './components/skeleton-screens/skeleton-list/skeleton-list.component';
+import { SkeletonDetailsComponent } from './components/skeleton-screens/skeleton-details/skeleton-details.component';
 
 const modules = [SharedModule, SalesRoutingModule];
 
 const components = [DeliveriesComponent, SalesComponent, SalesDetailsComponent];
 
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, OrdersListComponent, OrderDetailsComponent, SkeletonListComponent, SkeletonDetailsComponent],
   imports: [...modules],
 })
 export class SalesModule {}
