89bc5f96a1b1fb8d41e00afa36d70e1836ea440d
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-18 08:14:07 -0400

    - Creados componentes para mostrar mientras la informacion se carga.

---
+++ src/app/vistas-privadas/panel/views/sales/components/skeleton-screens/skeleton-list/skeleton-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-list',
+  templateUrl: './skeleton-list.component.html',
+  styleUrls: ['./skeleton-list.component.scss']
+})
+export class SkeletonListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
