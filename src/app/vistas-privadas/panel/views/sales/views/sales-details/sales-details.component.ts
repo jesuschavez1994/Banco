c63d4b3580295a6ad5db14d7e4f48be14fedf88f
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-19 08:35:35 -0400

    - Agregada funcion para realizar los calculos de los montos totales y el impuesto. - Se movio a este archivo para poder realizar el calculo cada vez que cambia la orden seleccionada.

--- src/app/vistas-privadas/panel/views/sales/views/sales-details/sales-details.component.ts
+++ src/app/vistas-privadas/panel/views/sales/views/sales-details/sales-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {
   StoreSalesService,
   Orders,
@@ -11,24 +11,62 @@ import {
   styleUrls: ['./sales-details.component.scss'],
 })
 export class SalesDetailsComponent implements OnInit {
-  storeOrders: Datum;
-  selectedOrderDetails: Orders;
+  @Input() taxPercentage = 19;
+
+  storeOrders: Datum[];
+  selectedOrderDetails: Datum;
+  totalAmount: number;
+  taxAmount: number;
   loading = true;
 
   constructor(private _storeSalesService: StoreSalesService) {}
 
   ngOnInit(): void {
     this._storeSalesService.getStoreOrders().subscribe((allOrders: Orders) => {
-      console.log('All the store orders');
-      console.log(allOrders);
       this.storeOrders = allOrders.data;
 
       this.selectedOrderDetails = this.storeOrders[0];
+      this.taxAmount = this.totalAmounts(this.storeOrders[0], 'tax');
+      this.totalAmount = this.totalAmounts(this.storeOrders[0], '');
       this.loading = false;
     });
   }
 
-  public getOrderDetails(orderDetails: Orders) {
+  public getOrderDetails(orderDetails: Datum) {
     this.selectedOrderDetails = orderDetails;
+    this.taxAmount = this.totalAmounts(orderDetails, 'tax');
+    this.totalAmount = this.totalAmounts(orderDetails, '');
+  }
+
+  private totalAmounts(
+    selectedOrder: Datum,
+    nameValueToGet: string = ''
+  ): number {
+    let subTotal = 0,
+      subTotalDelivery = 0,
+      tax = 0,
+      total = 0,
+      taxTotalByProduct = 0;
+
+    selectedOrder.order_cart_items.forEach((order) => {
+      subTotal += order.price * order.quantity;
+      taxTotalByProduct +=
+        order.price * order.quantity * (this.taxPercentage / 100);
+    });
+
+    subTotal += subTotalDelivery;
+    tax = taxTotalByProduct + subTotalDelivery * (this.taxPercentage / 100);
+    total = tax + subTotal;
+
+    switch (nameValueToGet) {
+      case 'tax':
+        return tax;
+
+      case 'delivery':
+        return subTotalDelivery;
+
+      default:
+        return total;
+    }
   }
 }
