89db8d1442f6477cb2526953bf1f76f6ae6b4f5e
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-18 23:53:20 -0400

    - Creado metodo para calcular el monto total de la factura incluyedo el impuesto.

--- src/app/vistas-privadas/panel/views/sales/components/order-details/order-details.component.ts
+++ src/app/vistas-privadas/panel/views/sales/components/order-details/order-details.component.ts
@@ -8,8 +8,44 @@ import { Orders, Datum } from '@services/store-sales/store-sales.service';
 })
 export class OrderDetailsComponent implements OnInit {
   @Input() orderDetails: Datum;
+  @Input() taxPercentage = 19;
+
+  taxAmount: number;
+  totalAmount: number;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taxAmount = this.totalAmounts('tax');
+    this.totalAmount = this.totalAmounts('');
+  }
+
+  private totalAmounts(nameValueToGet: string = ''): number {
+    let subTotal = 0,
+      subTotalDelivery = 0,
+      tax = 0,
+      total = 0,
+      taxTotalByProduct = 0;
+
+    this.orderDetails.order_cart_items.forEach((order) => {
+      subTotal += order.price * order.quantity;
+      taxTotalByProduct +=
+        order.price * order.quantity * (this.taxPercentage / 100);
+    });
+
+    subTotal += subTotalDelivery;
+    tax = taxTotalByProduct + subTotalDelivery * (this.taxPercentage / 100);
+    total = tax + subTotal;
+
+    switch (nameValueToGet) {
+      case 'tax':
+        return tax;
+
+      case 'delivery':
+        return subTotalDelivery;
+
+      default:
+        return total;
+    }
+  }
 }
