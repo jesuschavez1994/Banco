44832b1fd3ac9bc43136539aeee79449775a9123
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-19 08:33:46 -0400

    - Movida funcion para el calculo de los valores de impuesto y total a pagar.

--- src/app/vistas-privadas/panel/views/sales/components/order-details/order-details.component.ts
+++ src/app/vistas-privadas/panel/views/sales/components/order-details/order-details.component.ts
@@ -8,44 +8,10 @@ import { Orders, Datum } from '@services/store-sales/store-sales.service';
 })
 export class OrderDetailsComponent implements OnInit {
   @Input() orderDetails: Datum;
-  @Input() taxPercentage = 19;
-
-  taxAmount: number;
-  totalAmount: number;
+  @Input() totalAmount: number;
+  @Input() taxAmount: number;
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.taxAmount = this.totalAmounts('tax');
-    this.totalAmount = this.totalAmounts('');
-  }
-
-  private totalAmounts(nameValueToGet: string = ''): number {
-    let subTotal = 0,
-      subTotalDelivery = 0,
-      tax = 0,
-      total = 0,
-      taxTotalByProduct = 0;
-
-    this.orderDetails.order_cart_items.forEach((order) => {
-      subTotal += order.price * order.quantity;
-      taxTotalByProduct +=
-        order.price * order.quantity * (this.taxPercentage / 100);
-    });
-
-    subTotal += subTotalDelivery;
-    tax = taxTotalByProduct + subTotalDelivery * (this.taxPercentage / 100);
-    total = tax + subTotal;
-
-    switch (nameValueToGet) {
-      case 'tax':
-        return tax;
-
-      case 'delivery':
-        return subTotalDelivery;
-
-      default:
-        return total;
-    }
-  }
+  ngOnInit(): void {}
 }
