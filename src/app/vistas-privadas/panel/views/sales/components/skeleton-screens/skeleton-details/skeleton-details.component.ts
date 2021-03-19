b1cd013d7b991408d51e6c6b5257ab397890b60b
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-18 23:52:26 -0400

    - Toques finales a los componentes skeleton. - Añadidas animaciones.

--- src/app/vistas-privadas/panel/views/sales/components/skeleton-screens/skeleton-details/skeleton-details.component.ts
+++ src/app/vistas-privadas/panel/views/sales/components/skeleton-screens/skeleton-details/skeleton-details.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skeleton-details',
   templateUrl: './skeleton-details.component.html',
-  styleUrls: ['./skeleton-details.component.scss']
+  styleUrls: ['./skeleton-details.component.scss'],
 })
 export class SkeletonDetailsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
