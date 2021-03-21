610939fa2c0fa7f55153cd54430bdf9670f8c4c5
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-20 22:52:04 -0400

    - Creado componente dedicado para mostrar los detalles de los deliveries pendientes y completados.

---
+++ src/app/vistas-privadas/panel/pages/sales/components/deliveries-list/deliveries-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deliveries-list',
+  templateUrl: './deliveries-list.component.html',
+  styleUrls: ['./deliveries-list.component.scss']
+})
+export class DeliveriesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
