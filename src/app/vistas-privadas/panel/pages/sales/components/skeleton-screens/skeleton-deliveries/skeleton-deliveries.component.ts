34ce9e5dd434a106a5fe7a73a25650c6b1d1773b
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-20 22:51:28 -0400

    - Creado componente dedicado para la animacion de carga en la vista.

---
+++ src/app/vistas-privadas/panel/pages/sales/components/skeleton-screens/skeleton-deliveries/skeleton-deliveries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-deliveries',
+  templateUrl: './skeleton-deliveries.component.html',
+  styleUrls: ['./skeleton-deliveries.component.scss']
+})
+export class SkeletonDeliveriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
