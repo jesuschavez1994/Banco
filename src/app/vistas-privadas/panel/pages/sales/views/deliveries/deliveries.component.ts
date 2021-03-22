a36568abc783a93cbfe9c8d4172b239735e0358e
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-22 18:22:11 -0400

    - Peticion de informacion de los pedidos. - Agregados estilos.

--- src/app/vistas-privadas/panel/pages/sales/views/deliveries/deliveries.component.ts
+++ src/app/vistas-privadas/panel/pages/sales/views/deliveries/deliveries.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreSalesService } from '@services/store-sales/store-sales.service';
+import {
+  Orders,
+  Datum,
+} from '@interfaces/shopping-cart/shopping-cart.interface';
 
 @Component({
   selector: 'app-deliveries',
   templateUrl: './deliveries.component.html',
-  styleUrls: ['./deliveries.component.scss']
+  styleUrls: ['./deliveries.component.scss'],
 })
 export class DeliveriesComponent implements OnInit {
+  deliveryOrders: Datum[];
+  loadingData = true;
 
-  constructor() { }
+  constructor(private _storeSalesService: StoreSalesService) {}
 
   ngOnInit(): void {
+    this._storeSalesService.getStoreOrders().subscribe((ordersInfo: Orders) => {
+      this.deliveryOrders = ordersInfo.data;
+      this.loadingData = false;
+    });
   }
-
 }
