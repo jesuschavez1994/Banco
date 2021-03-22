5ed55bfe4bb731bfe320f3841808d1d81dd4bb2f
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-22 18:25:57 -0400

    - Añadido metodo para calcular la cantidad de articulos del carrito.

--- src/app/vistas-privadas/panel/pages/sales/components/deliveries-list/deliveries-list.component.ts
+++ src/app/vistas-privadas/panel/pages/sales/components/deliveries-list/deliveries-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {
+  Orders,
+  Datum,
+} from '@interfaces/shopping-cart/shopping-cart.interface';
 
 @Component({
   selector: 'app-deliveries-list',
   templateUrl: './deliveries-list.component.html',
-  styleUrls: ['./deliveries-list.component.scss']
+  styleUrls: ['./deliveries-list.component.scss'],
 })
 export class DeliveriesListComponent implements OnInit {
+  @Input() orderInfo: Datum;
 
-  constructor() { }
+  itemsCount = 0;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.calculateCartItems(this.orderInfo);
   }
 
+  private calculateCartItems(order: Datum) {
+    order.order_cart_items.forEach((item) => {
+      this.itemsCount += item.quantity;
+    });
+  }
 }
