7186497408dfa7923bc3a57dac78e63c5fb664f8
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-22 18:18:59 -0400

    - Organizacion de codigo.

--- src/app/vistas-privadas/panel/pages/sales/sales.module.ts
+++ src/app/vistas-privadas/panel/pages/sales/sales.module.ts
@@ -9,13 +9,25 @@ import { OrdersListComponent } from './components/orders-list/orders-list.compon
 import { OrderDetailsComponent } from './components/order-details/order-details.component';
 import { SkeletonListComponent } from './components/skeleton-screens/skeleton-list/skeleton-list.component';
 import { SkeletonDetailsComponent } from './components/skeleton-screens/skeleton-details/skeleton-details.component';
+import { DeliveriesListComponent } from './components/deliveries-list/deliveries-list.component';
+import { SkeletonDeliveriesComponent } from './components/skeleton-screens/skeleton-deliveries/skeleton-deliveries.component';
 
 const modules = [SharedModule, SalesRoutingModule];
 
-const components = [DeliveriesComponent, SalesComponent, SalesDetailsComponent];
+const components = [
+  DeliveriesComponent,
+  SalesComponent,
+  SalesDetailsComponent,
+  OrdersListComponent,
+  OrderDetailsComponent,
+  SkeletonListComponent,
+  SkeletonDetailsComponent,
+  DeliveriesListComponent,
+  SkeletonDeliveriesComponent,
+];
 
 @NgModule({
-  declarations: [...components, OrdersListComponent, OrderDetailsComponent, SkeletonListComponent, SkeletonDetailsComponent],
+  declarations: [...components],
   imports: [...modules],
 })
 export class SalesModule {}
