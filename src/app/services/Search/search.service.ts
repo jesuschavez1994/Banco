353b15382fa47e90b55049c4f5087e25667646ef
Jacinto Acosta <jacintoaczz@gmail.com>
2021-03-25 12:39:11 -0400

    Organizacion de codigo. Eliminados console.log().

--- src/app/services/Search/search.service.ts
+++ src/app/services/Search/search.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core'
-import { Service } from '@services/service.service'
+import { Injectable } from '@angular/core';
+import { Service } from '@services/service.service';
 import {
   HttpClient,
   HttpHeaders,
   HttpErrorResponse,
-} from '@angular/common/http'
+} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SearchService extends Service {
   constructor(protected http: HttpClient) {
-    super(http)
+    super(http);
   }
 
   // ================================= //
@@ -19,8 +19,8 @@ export class SearchService extends Service {
   // ================================= //
 
   SearchProductStore(userId: string, storeId: string, data: any) {
-    const url = `users/${userId}/stores/${storeId}/products_search`
-    return this.postQuery(url, data)
+    const url = `users/${userId}/stores/${storeId}/products_search`;
+    return this.postQuery(url, data);
   }
 
   globalProductSearch(searchFilters: any, page: number = 1) {
@@ -29,10 +29,9 @@ export class SearchService extends Service {
       headers: new HttpHeaders({
         ContentType: 'application/json',
       }),
-    }
-    console.log('Actual page:', page)
-    console.log('Filters:', searchFilters)
-    const url = `product_search_filters`
-    return this.postQuery(url, searchFilters, true, httpOptions)
+    };
+
+    const url = `product_search_filters`;
+    return this.postQuery(url, searchFilters, true, httpOptions);
   }
 }
