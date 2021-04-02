f8148d3d83fd839cdcedc27938239e2d268ee7ce
Jacinto Acosta <jacintoaczz@gmail.com>
2021-04-02 17:20:40 -0400

    - Creacion de componente skeleton para el editor de ordenes.

---
+++ src/app/vistas-privadas/panel/pages/shopping-cart/components/skeleton/skeleton-editor/skeleton-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-editor',
+  templateUrl: './skeleton-editor.component.html',
+  styleUrls: ['./skeleton-editor.component.scss']
+})
+export class SkeletonEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
