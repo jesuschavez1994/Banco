bb003a31bc3f6c80a9b8ad7e85f7878016805f6d
Jacinto Acosta <jacintoaczz@gmail.com>
2021-04-01 23:19:46 -0400

    - Ajustes en los estilos.

--- src/app/vistas-privadas/panel/pages/sales/components/deliveries-list/deliveries-list.component.ts
+++ src/app/vistas-privadas/panel/pages/sales/components/deliveries-list/deliveries-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 import {
   Orders,
   Datum,
@@ -13,11 +14,18 @@ export class DeliveriesListComponent implements OnInit {
   @Input() orderInfo: Datum;
 
   itemsCount = 0;
+  avatarBackground: string | SafeStyle;
 
-  constructor() {}
+  constructor(private sanitization: DomSanitizer) {}
 
   ngOnInit(): void {
     this.calculateCartItems(this.orderInfo);
+
+    this.orderInfo.user.image[0].src
+      ? (this.avatarBackground = this.sanitization.bypassSecurityTrustStyle(
+          `url('https://founduss.cl/${this.orderInfo.user.image[0].src}')`
+        ))
+      : (this.avatarBackground = `url('assets/img/no-avatar.jpg')`);
   }
 
   private calculateCartItems(order: Datum) {
